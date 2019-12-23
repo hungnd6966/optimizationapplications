948b54466ba61a4d13855b0b94b3599fa2d1b8ca
dungkhmt <dungkhmt@gmail.com>
2019-12-23 16:59:51 +0700

    init optapp

---
+++ optapp/src/main/java/com/hust/opt/optapp/App.java
@@ -0,0 +1,13 @@
+package com.hust.opt.optapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
